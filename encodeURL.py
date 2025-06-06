264b989cdf77cb7d2bb676179a1a7ea1d1e4b8a2
R0gfall <kornilovleo@ya.ru>
2025-06-06 19:43:57 +0300

    Add ....// and ..\/

--- encodeURL.py
+++ encodeURL.py
@@ -1,21 +1,18 @@
 import urllib.parse 
 
-
-
 # По сути можно сделать два варианта, первый берет на вход только одно слово и его кодирует в список
 # Второй вариант - можно передать какой нибудь флаг и бахнуть словарь == ЕГО ПОЗЖЕ реализую
-
 
 def encode_word_to_url(word: str):
     
     payload = '..'
     variants = []
-    # Классические слэши
+    # /
     variants.append(payload + '/')                   # .. /
     variants.append(payload + '%c0%af')              # ..%c0%af (overlong)
     variants.append(payload + '%ef%bc%8f')           # ..%ef%bc%8f (full-width)
 
-    # Обратный слэш
+    # \
     variants.append(payload + '\\')                  # ..\
     variants.append(payload + '%5c')                 # ..%5c
     variants.append(payload + '%c0%5c')              # ..%c0%5c (примерно)
@@ -41,31 +38,29 @@ def encode_word_to_url(word: str):
             file_output.write(f"{encoded}\n")
 
 
-
-
         
         encoded = urllib.parse.quote(encoded, safe="")
-
-
-
 
         for i in variants:
             output_word = i.join(word.split("../"))
             file_output.write(f"{output_word}\n")
 
-    
+
+        # Добавление ....//
+
+        output_word = "....//".join(word.split("../"))
+        file_output.write(f"{output_word}\n")
+
+        # Добавление ..\/
+
+        output_word = "..\\/".join(word.split("../"))
+        file_output.write(f"{output_word}\n")
+
+    # print(encoded)
+    # print()
+
 
     
-    
-
-    print(encoded)
-    #print()
-
-
-    
-
-
-
 
 
 if __name__ == "__main__":
